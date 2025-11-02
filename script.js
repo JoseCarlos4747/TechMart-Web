f8e4ffb5444928e92bcccc6886ca921e333a1083
Critan15 <74818615@continental.edu.pe>
2025-11-02 10:26:32 -0500

    avance

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //Pantalla para pago
 const Pago=document.getElementById("Pago");
+//const para evitar posibles errores tecnicos, tambien se puede usar let
 
 
 
@@ -21,6 +22,9 @@ const BotonConfirmar_pago_4=document.getElementById("ConfirmarPago");
 
 const YAPE=document.getElementById("Metodo_Imagenes");
 
+
+
+
 BotonAbrir_pago.addEventListener("click",function()
 //el addEventlistener es para escuchar eventos como clicks, movimientos del mouse, etc
 {
@@ -37,7 +41,7 @@ BotonCerrar_pago.addEventListener("click",function()
     Detalle_pago.style.display="none";
     general.style.filter="none";
 });
-window.onclick=e=>{if(e.target==Pago) Pago.style.display="none";}
+
 
 
 BotonContinuar_pago_1.addEventListener("click",function()
@@ -50,9 +54,22 @@ BotonAtras_pago_2.addEventListener("click",function()
 {
     Detalle_pago.style.display="block";
     Info_pago.style.display="none";
+    
 })
-BotonContinuar_pago_2.addEventListener("click",function()
+BotonContinuar_pago_2.addEventListener("click",function(event)
 {
+
+    event.preventDefault();//Para que no se cierre esta ventana, evitar enviar el formulario y que se recargue la pagina
+    const Input_departamento=document.getElementById("Departamento").value.trim();//trim para omitir espacios
+    const Input_nombre=document.getElementById("name").value.trim();
+    const Input_apellidopaterno=document.getElementById("apellido_paterno").value.trim();
+    const Input_apellidomaterno=document.getElementById("apellido_materno").value.trim();
+    const Input_telefono=document.getElementById("telefono").value.trim();
+    if(Input_departamento==="" || Input_nombre==="" || Input_apellidopaterno==="" || Input_apellidomaterno==="" || Input_telefono==="")
+    {
+        alert("Error, campos vacios");
+        return;//para que no pase a la siguiente ventana
+    }
     Metodo_pago.style.display="flex";
     Info_pago.style.display="none";
 });
@@ -62,8 +79,18 @@ BotonAtras_pago_3.addEventListener("click",function()
     Info_pago.style.display="block";
     YAPE.style.display="none";
 });
-BotonContinuar_pago_3.addEventListener("click",function()
+BotonContinuar_pago_3.addEventListener("click",function(event)
 {
+    event.preventDefault();//Para que no se cierre esta ventana, evitar enviar el formulario y que se recargue la pagina
+    const Input_radio_yape=document.getElementById("yape");
+    const Input_radio_tarjeta=document.getElementById("tarjeta");
+    const Input_radio_Entrega=document.getElementById("ConEntrega");
+
+    if(!Input_radio_yape.checked && !Input_radio_tarjeta.checked && !Input_radio_Entrega.checked)
+    {
+        alert("Error, seleccione un campo");
+        return;
+    }
     YAPE.style.display="block";
 });
 
