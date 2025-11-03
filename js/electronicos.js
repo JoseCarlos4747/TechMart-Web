2842ddde60d1d648461d07f759d9d99e1bcc65cd
Alim930 <73125276@continental.edu.pe>
2025-11-03 05:43:05 -0500

    cambios uqe olvide commitear

--- js/electronicos.js
+++ js/electronicos.js
@@ -3,6 +3,7 @@ const electronicos = [
         id: 1,
         marca: "NovaTech",
         nombre: "NovaTech SmartWatch Pro Active 1.78\" AMOLED GPS IP68",
+        categoria:"electronicos",
         img: "./img/producto_1.jpg",
         precio: 239.00
     },
@@ -10,6 +11,7 @@ const electronicos = [
         id: 2,
         marca: "EduTab",
         nombre: "EduTab Tablet 10.1\" FHD 6GB/128GB Wi-Fi + microSD",
+        categoria:"electronicos",
         img: "./img/producto_2.jpg",
         precio: 549.00
     },
@@ -17,6 +19,7 @@ const electronicos = [
         id: 3,
         marca: "Lumio",
         nombre: "Lumio MiniProyector LED 1080p 120\" HDMI/USB/BT",
+        categoria:"electronicos",
         img: "./img/producto_3.jpg",
         precio: 389.00
     },
@@ -24,6 +27,7 @@ const electronicos = [
         id: 4,
         marca: "UltraCast",
         nombre: "UltraCast TV Box 4K Ultra Connect Wi-Fi ac BT",
+        categoria:"electronicos",
         img: "./img/producto_4.webp",
         precio: 199.00
     },
@@ -31,6 +35,7 @@ const electronicos = [
         id: 5,
         marca: "CaseShield",
         nombre: "Funda Antigolpes Slim con Aro Magnético (MagSafe) para Smartphone",
+        categoria:"accesorios",
         img: "./img/producto_5.webp",
         precio: 49.00
     },
@@ -38,6 +43,7 @@ const electronicos = [
         id: 6,
         marca: "ClearGuard",
         nombre: "Protector de Pantalla 3D Hidrogel (Auto-regeneración, instalación fácil)",
+        categoria:"accesorios",
         img: "./img/producto_6.jpg",
         precio: 29.00
     },
@@ -45,6 +51,7 @@ const electronicos = [
         id: 7,
         marca: "VoltLine",
         nombre: "Cable USB-C a USB-C 100W PD Trenzado 1.8 m (Carga rápida + Datos)",
+        categoria:"accesorios",
         img: "./img/producto_7.jpg",
         precio: 39.00
     },
@@ -52,6 +59,7 @@ const electronicos = [
         id: 8,
         marca: "LiftUp",
         nombre: "Soporte de Laptop Aluminio Ajustable (11\"–16\", disipación y plegable)",
+        categoria:"accesorios",
         img: "./img/producto_8.jpg",
         precio: 89.00
     },
@@ -59,6 +67,7 @@ const electronicos = [
         id: 9,
         marca: "BreezeLite",
         nombre: "Mini Ventilador de Escritorio USB Silencioso con 3 Velocidades",
+        categoria:"gadgets",
         img: "./img/producto_9.webp",
         precio: 59.00
     },
@@ -66,6 +75,7 @@ const electronicos = [
         id: 10,
         marca: "SnapCam",
         nombre: "Mini Cámara de Seguridad Wi-Fi 1080p con Detección de Movimiento",
+        categoria:"gadgets",
         img: "./img/producto_10.jpg",
         precio: 119.00
     },
@@ -73,6 +83,7 @@ const electronicos = [
         id: 11,
         marca: "SmartThermo",
         nombre: "Termómetro/Higrómetro Inteligente BT con App y Alertas",
+        categoria:"gadgets",
         img: "./img/producto_11.webp",
         precio: 69.00
     },
@@ -80,6 +91,7 @@ const electronicos = [
         id: 12,
         marca: "TrackLight",
         nombre: "Linterna COB Recargable con Imán y Gancho",
+        categoria:"gadgets",
         img: "./img/producto_12.webp",
         precio: 49.00
     }
